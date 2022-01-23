b996a86981083142670bfae8dd978571b569c57e
xstazio <xstazio@gmail.com>
2022-01-24 00:14:38 +0300

    Обновление компонентов

--- gulpfile.js
+++ gulpfile.js
@@ -1,144 +1,160 @@
 // generated on 2021-02-19 using generator-webapp 4.0.0-8
-const { src, dest, watch, series, parallel, lastRun } = require('gulp');
-const gulpLoadPlugins = require('gulp-load-plugins');
-const fs = require('fs');
-const mkdirp = require('mkdirp');
-const Modernizr = require('modernizr');
-const browserSync = require('browser-sync');
-const del = require('del');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const { argv } = require('yargs');
+const { src, dest, watch, series, parallel, lastRun } = require("gulp");
+const gulpLoadPlugins = require("gulp-load-plugins");
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+const Modernizr = require("modernizr");
+const browserSync = require("browser-sync");
+const del = require("del");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const { argv } = require("yargs");
 
 const $ = gulpLoadPlugins();
 const server = browserSync.create();
 
 const port = argv.port || 9000;
 
-const isProd = process.env.NODE_ENV === 'production';
-const isTest = process.env.NODE_ENV === 'test';
+const isProd = process.env.NODE_ENV === "production";
+const isTest = process.env.NODE_ENV === "test";
 const isDev = !isProd && !isTest;
+const sass = require("gulp-sass")(require("sass"));
 
 function styles() {
-  return src('app/styles/*.scss', {
+  return src("app/styles/*.scss", {
     sourcemaps: !isProd,
   })
     .pipe($.plumber())
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe($.postcss([
-      autoprefixer()
-    ]))
-    .pipe(dest('.tmp/styles', {
-      sourcemaps: !isProd,
-    }))
-    .pipe(server.reload({stream: true}));
-};
+    .pipe(
+      sass
+        .sync({
+          outputStyle: "expanded",
+          precision: 10,
+          includePaths: ["."],
+        })
+        .on("error", sass.logError)
+    )
+    .pipe($.postcss([autoprefixer()]))
+    .pipe(
+      dest(".tmp/styles", {
+        sourcemaps: !isProd,
+      })
+    )
+    .pipe(server.reload({ stream: true }));
+}
 
 function scripts() {
-  return src('app/scripts/**/*.js', {
+  return src("app/scripts/**/*.js", {
     sourcemaps: !isProd,
   })
     .pipe($.plumber())
     .pipe($.babel())
-    .pipe(dest('.tmp/scripts', {
-      sourcemaps: !isProd ? '.' : false,
-    }))
-    .pipe(server.reload({stream: true}));
-};
+    .pipe(
+      dest(".tmp/scripts", {
+        sourcemaps: !isProd ? "." : false,
+      })
+    )
+    .pipe(server.reload({ stream: true }));
+}
 
 async function modernizr() {
-  const readConfig = () => new Promise((resolve, reject) => {
-    fs.readFile(`${__dirname}/modernizr.json`, 'utf8', (err, data) => {
-      if (err) reject(err);
-      resolve(JSON.parse(data));
-    })
-  })
-  const createDir = () => new Promise((resolve, reject) => {
-    mkdirp(`${__dirname}/.tmp/scripts`, err => {
-      if (err) reject(err);
-      resolve();
-    })
-  });
-  const generateScript = config => new Promise((resolve, reject) => {
-    Modernizr.build(config, content => {
-      fs.writeFile(`${__dirname}/.tmp/scripts/modernizr.js`, content, err => {
+  const readConfig = () =>
+    new Promise((resolve, reject) => {
+      fs.readFile(`${__dirname}/modernizr.json`, "utf8", (err, data) => {
         if (err) reject(err);
-        resolve(content);
+        resolve(JSON.parse(data));
       });
-    })
-  });
+    });
+  const createDir = () =>
+    new Promise((resolve, reject) => {
+      mkdirp(`${__dirname}/.tmp/scripts`, (err) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });
+  const generateScript = (config) =>
+    new Promise((resolve, reject) => {
+      Modernizr.build(config, (content) => {
+        fs.writeFile(
+          `${__dirname}/.tmp/scripts/modernizr.js`,
+          content,
+          (err) => {
+            if (err) reject(err);
+            resolve(content);
+          }
+        );
+      });
+    });
 
-  const [config] = await Promise.all([
-    readConfig(),
-    createDir()
-  ]);
+  const [config] = await Promise.all([readConfig(), createDir()]);
   await generateScript(config);
 }
 
 const lintBase = (files, options) => {
   return src(files)
     .pipe($.eslint(options))
-    .pipe(server.reload({stream: true, once: true}))
+    .pipe(server.reload({ stream: true, once: true }))
     .pipe($.eslint.format())
     .pipe($.if(!server.active, $.eslint.failAfterError()));
-}
+};
 function lint() {
-  return lintBase('app/scripts/**/*.js', { fix: true })
-    .pipe(dest('app/scripts'));
-};
+  return lintBase("app/scripts/**/*.js", { fix: true }).pipe(
+    dest("app/scripts")
+  );
+}
 function lintTest() {
-  return lintBase('test/spec/**/*.js');
-};
+  return lintBase("test/spec/**/*.js");
+}
 
 function html() {
-  return src('app/*.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
-    .pipe($.if(/\.css$/, $.postcss([cssnano({safe: true, autoprefixer: false})])))
-    .pipe($.if(/\.html$/, $.htmlmin({
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: {compress: {drop_console: true}},
-      processConditionalComments: true,
-      removeComments: true,
-      removeEmptyAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true
-    })))
-    .pipe(dest('dist'));
+  return src("app/*.html")
+    .pipe($.useref({ searchPath: [".tmp", "app", "."] }))
+    .pipe($.if(/\.js$/, $.uglify({ compress: { drop_console: true } })))
+    .pipe(
+      $.if(/\.css$/, $.postcss([cssnano({ safe: true, autoprefixer: false })]))
+    )
+    .pipe(
+      $.if(
+        /\.html$/,
+        $.htmlmin({
+          collapseWhitespace: true,
+          minifyCSS: true,
+          minifyJS: { compress: { drop_console: true } },
+          processConditionalComments: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+        })
+      )
+    )
+    .pipe(dest("dist"));
 }
 
 function images() {
-  return src('app/images/**/*', { since: lastRun(images) })
+  return src("app/images/**/*", { since: lastRun(images) })
     .pipe($.imagemin())
-    .pipe(dest('dist/images'));
-};
+    .pipe(dest("dist/images"));
+}
 
 function fonts() {
-  return src('app/fonts/**/*.{eot,svg,ttf,woff,woff2}')
-    .pipe($.if(!isProd, dest('.tmp/fonts'), dest('dist/fonts')));
-};
+  return src("app/fonts/**/*.{eot,svg,ttf,woff,woff2}").pipe(
+    $.if(!isProd, dest(".tmp/fonts"), dest("dist/fonts"))
+  );
+}
 
 function extras() {
-  return src([
-    'app/*',
-    '!app/*.html'
-  ], {
-    dot: true
-  }).pipe(dest('dist'));
-};
+  return src(["app/*", "!app/*.html"], {
+    dot: true,
+  }).pipe(dest("dist"));
+}
 
 function clean() {
-  return del(['.tmp', 'dist'])
+  return del([".tmp", "dist"]);
 }
 
 function measureSize() {
-  return src('dist/**/*')
-    .pipe($.size({title: 'build', gzip: true}));
+  return src("dist/**/*").pipe($.size({ title: "build", gzip: true }));
 }
 
 const build = series(
@@ -158,23 +174,22 @@ function startAppServer() {
     notify: false,
     port,
     server: {
-      baseDir: ['.tmp', 'app'],
+      baseDir: [".tmp", "app"],
       routes: {
-        '/node_modules': 'node_modules'
-      }
-    }
+        "/node_modules": "node_modules",
+      },
+    },
   });
 
-  watch([
-    'app/*.html',
-    'app/images/**/*',
-    '.tmp/fonts/**/*'
-  ]).on('change', server.reload);
+  watch(["app/*.html", "app/images/**/*", ".tmp/fonts/**/*"]).on(
+    "change",
+    server.reload
+  );
 
-  watch('app/styles/**/*.scss', styles);
-  watch('app/scripts/**/*.js', scripts);
-  watch('modernizr.json', modernizr);
-  watch('app/fonts/**/*', fonts);
+  watch("app/styles/**/*.scss", styles);
+  watch("app/scripts/**/*.js", scripts);
+  watch("modernizr.json", modernizr);
+  watch("app/fonts/**/*", fonts);
 }
 
 function startTestServer() {
@@ -183,17 +198,17 @@ function startTestServer() {
     port,
     ui: false,
     server: {
-      baseDir: 'test',
+      baseDir: "test",
       routes: {
-        '/scripts': '.tmp/scripts',
-        '/node_modules': 'node_modules'
-      }
-    }
+        "/scripts": ".tmp/scripts",
+        "/node_modules": "node_modules",
+      },
+    },
   });
 
-  watch('test/index.html').on('change', server.reload);
-  watch('app/scripts/**/*.js', scripts);
-  watch('test/spec/**/*.js', lintTest);
+  watch("test/index.html").on("change", server.reload);
+  watch("app/scripts/**/*.js", scripts);
+  watch("test/spec/**/*.js", lintTest);
 }
 
 function startDistServer() {
@@ -201,17 +216,21 @@ function startDistServer() {
     notify: false,
     port,
     server: {
-      baseDir: 'dist',
+      baseDir: "dist",
       routes: {
-        '/node_modules': 'node_modules'
-      }
-    }
+        "/node_modules": "node_modules",
+      },
+    },
   });
 }
 
 let serve;
 if (isDev) {
-  serve = series(clean, parallel(styles, scripts, modernizr, fonts), startAppServer);
+  serve = series(
+    clean,
+    parallel(styles, scripts, modernizr, fonts),
+    startAppServer
+  );
 } else if (isTest) {
   serve = series(clean, scripts, startTestServer);
 } else if (isProd) {
